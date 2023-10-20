4fe78b85566a26cd66ac3a0a2b295b2308b0706e
Luis Montes <monteslu@gmail.com>
2023-10-19 18:36:31 -0700

    p2p nodes

--- hsync.js
+++ hsync.js
@@ -1,52 +1,60 @@
 const { createConnection } = require('hsync');
+const queryString = require('node:querystring');
 
 module.exports = function(RED) {
   const cons = {};
+  const peerCons = {};
 
   function HsyncConnection(config) {
     RED.nodes.createNode(this, config);
     const node = this;
     const { hsyncSecret } = node.credentials;
     const { hsyncServer } = config;
+    node.hsyncServer = hsyncServer;
+    const hsu = new URL((hsyncServer + '').trim().toLowerCase());
+    node.hsu = hsu;
 
     // console.log('hsyncServer', config, node.credentials, 'con', !!node.con);
-    let keepCon = false;
-    const oldCon = cons[hsyncServer];
-    if (oldCon &&
-        oldCon.config.hsyncSecret === hsyncSecret && 
-        oldCon.config.hsyncServer === hsyncServer && 
-        oldCon.config.localHost === config.localWebHost &&
-        oldCon.config.port === config.localWebPort) {
-        keepCon = true;
-    }
+    
+    const oldCon = cons[hsu.hostname];
+    const keepCon = (
+      oldCon &&
+      oldCon.config.hsyncSecret === hsyncSecret && 
+      oldCon.config.hsyncServer === hsyncServer && 
+      oldCon.config.localHost === config.localWebHost &&
+      oldCon.config.port === config.localWebPort
+    );
+
+    const connectedFunc = () => {
+      node.emit('hsync_connected');
+    };
+
+    const errorFunc = (e) => {
+      console.error('hsync_error', e);
+      node.emit('hysnc_error', e);
+    };
+
+    node.on('close', (removed, done) => {
+      node.con.removeListener('connected', connectedFunc);
+      node.con.removeListener('error', errorFunc);
+      if (removed) {
+        node.con.endClient(true);
+        delete cons[hsu.hostname];
+        console.log('oldcon node removed');
+      } else {
+        // This node is being restarted
+        console.log('oldcon node restarted');
+      }
+      done();
+    });
 
     if (keepCon) {
-      // console.log('keep con');
       node.con = oldCon;
-  
-      node.con.on('error', (e) => {
-        console.log('error', e);
-      });
-
-      node.on('input', function(msg) {
-        msg.payload = msg.payload.toLowerCase();
-        node.send(msg);
-      });
-      node.on('close', function(removed, done) {
-        if (removed) {
-            node.con.endClient(true);
-            delete cons[hsyncServer];
-            console.log('oldcon node removed');
-        } else {
-            // This node is being restarted
-            console.log('oldcon node restarted');
-        }
-        done();
-      });
+      node.con.on('error', errorFunc);
     } else {
       if (oldCon) {
         oldCon.endClient(true);
-        delete cons[hsyncServer];
+        delete cons[hsu.hostname];
       }
       const conOptions = {
         hsyncServer,
@@ -54,32 +62,16 @@ module.exports = function(RED) {
         localhost: config.localWebHost,
         port: config.localWebPort,
       };
+      
       createConnection(conOptions).then((con) => {
-  
+        // console.log('createConnection!!!!!!!!!!!!!!');
+        node.emit('hsync_connecting');
         node.con = con;
-        cons[hsyncServer] = con;
+        cons[hsu.hostname] = con;
   
-        // console.log('node.con', Object.keys(node.con));
+        node.con.on('error', errorFunc);
+        node.con.on('connected', connectedFunc);
   
-        node.con.on('error', (e) => {
-          console.log('error', e);
-        });
-  
-        node.on('input', function(msg) {
-          msg.payload = msg.payload.toLowerCase();
-          node.send(msg);
-        });
-        node.on('close', function(removed, done) {
-          if (removed) {
-              node.con.endClient(true);
-              delete cons[hsyncServer];
-              console.log('node removed');
-          } else {
-              // This node is being restarted
-              console.log('node restarted');
-          }
-          done();
-        });
       })
       .catch((e) => {
         console.log('error launching hsync', e, conOptions);
@@ -90,16 +82,30 @@ module.exports = function(RED) {
   }
   RED.nodes.registerType('hsync-connection', HsyncConnection, {
     credentials: {
-      hsyncServer: {type:'text',required:true},
       hsyncSecret: {type:'password',required:true}
     }
   });
+
+  function HsyncPeer(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const { host } = config;
+    const hsu = new URL((host + '').trim().toLowerCase());
+    node.hsu = hsu;
+
+    peerCons[hsu.hostname] = peerCons[hsu.hostname] || [];
+
+    node.host = host;
+    // console.log('HsyncPeer', config);
+
+  }
+  RED.nodes.registerType('hsync-peer', HsyncPeer);
   
   function HsyncOut(config) {
     RED.nodes.createNode(this, config);
     const node = this;
-    // const hsync = RED.nodes.getNode(config.connection);
-    node.host = config.host;
+    const hsyncPeer = RED.nodes.getNode(config.peer);
+    node.host = hsyncPeer.host;
 
     node.on('input', async function(msg) {
       let host = node.host;
@@ -108,8 +114,18 @@ module.exports = function(RED) {
       } else {
         host = host + '/_hs/message';
       }
+      
+      if (msg.query) {
+        const qs = queryString.encode(msg.query);
+        // console.log('hsync-out qs', qs);
+        if (qs) {
+          host = host + '?' + qs;
+        }
+      }
+      
       let status;
       try {
+        node.status({fill:'yellow',shape:'ring',text:'sending...'});
         const options = {
           method: 'POST',
           headers: {
@@ -127,9 +143,11 @@ module.exports = function(RED) {
         if (status >= 400) {
           throw new Error(text);
         }
+        node.status({fill:'green',shape:'dot',text:'sent'});
 
       } catch (e) {
         e.status = status;
+        node.status({fill:'red',shape:'ring',text:'error'});
         node.error(e);
       }
     });
@@ -141,17 +159,22 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     const node = this;
     const hsync = RED.nodes.getNode(config.connection);
-    
-    hsync.on('error', () => {
-      node.status({fill:'red',shape:'ring',text:'error'});
+
+    hsync.on('hsync_connecting', () => {
+      node.status({fill:'yellow',shape:'ring',text:'connecting...'});
     });
 
-    hsync.con.on('connected', () => {
+    hsync.on('hsync_connected', () => {
       let host = 'connected';
       try {
         host = new URL(hsync.con.hsyncServer).hostname;
       } catch (e) {}
       node.status({fill:'green',shape:'dot',text: host});
+    });
+    
+    hsync.on('hysnc_error', (e) => {
+      node.status({fill:'red',shape:'ring',text:'error'});
+      node.error(e);
     });
 
     hsync.con.on('json', (msg) => {
@@ -163,6 +186,178 @@ module.exports = function(RED) {
       node.send(msg);
     });
   }
-
   RED.nodes.registerType('hsync-in', HsyncIn);
+
+  function HsyncP2PIn(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const hsync = RED.nodes.getNode(config.connection);
+    const hsyncPeer = RED.nodes.getNode(config.peer);
+    const conShortName = hsync.hsu.hostname.split('.')[0];
+    const peerShortName = hsyncPeer.hsu.hostname.split('.')[0];
+
+    if (hsyncPeer.hsu.hostname === hsync.hsu.hostname) {
+      node.error('cannot connect to self');
+      node.status({fill:'red',shape:'ring',text:'cannot connect to self'});
+      return;
+    }
+    
+    const peer = hsync.con.getPeer(hsyncPeer.host);
+    peerCons[hsyncPeer.hsu.hostname] = peerCons[hsyncPeer.hsu.hostname] || peer;
+
+    // console.log('peer', peer, peerCons);
+    // console.log('HsyncP2PIn', hsyncPeer.host, '→', hsync.hsyncServer, peer.dcOpen, peer.rtcStatus);
+
+    if (peer.dcOpen) {
+      console.log('dcOpen already open', hsyncPeer.host);
+      const text = `${peerShortName} → ${conShortName}`;
+      node.status({fill:'green',shape:'dot',text});
+    }
+
+    peer.rtcEvents.on('dcOpen', () => { 
+      // console.log('dcOpen to', hsyncPeer.host);
+      const text = `${peerShortName} → ${conShortName}`;
+      node.status({fill:'green',shape:'dot',text});
+    });
+
+    if (peer.rtcStatus === 'error') {
+      // console.log('RTC ERROR', hsyncPeer.host);
+      node.status({fill:'red',shape:'ring',text:'error'});
+    }
+
+    if (peer.rtcStatus === 'connecting') {
+      // console.log('RTC CONNECTING', hsyncPeer.host);
+      node.status({fill:'yellow',shape:'ring',text:'connecting...'});
+    }
+
+    peer.rtcEvents.on('error', () => { 
+      node.status({fill:'red',shape:'dot',text:error.getMessage()});
+    });
+
+    peer.rtcEvents.on('closed', () => {
+      node.status({fill:'red',shape:'ring',text:'closed'});
+    });
+
+    peer.rtcEvents.on('disconnected', () => {
+      node.status({fill:'red',shape:'ring',text:'disconnected'});
+    });
+
+    peer.rtcEvents.on('jsonMsg', (msg) => {
+      node.send(msg);
+    });
+
+    if (!peer.dcOpen && peer.rtcStatus !== 'connecting') {
+      node.status({fill:'yellow',shape:'ring',text:'connecting...'});
+      console.log('connecting rtc');
+      peer.connectRTC()
+      .then(() => {
+        console.log('then, connected rtc');
+      })
+      .catch((e) => {
+        console.log('err, connect rtc', e);
+        node.error(e);
+      });
+    }
+
+    // console.log('peer', peer);
+
+    hsync.on('error', () => {
+      node.status({fill:'red',shape:'ring',text:'error'});
+    });
+
+    // hsync.con.on('connected', () => {
+    //   let host = 'connected';
+    //   try {
+    //     host = new URL(hsync.con.hsyncServer).hostname;
+    //   } catch (e) {}
+    //   node.status({fill:'green',shape:'dot',text: host});
+    // });
+
+  }
+  RED.nodes.registerType('hs-p2p-in', HsyncP2PIn);
+
+  function HsyncP2POut(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const hsync = RED.nodes.getNode(config.connection);
+    const hsyncPeer = RED.nodes.getNode(config.peer);
+    const conShortName = hsync.hsu.hostname.split('.')[0];
+    const peerShortName = hsyncPeer.hsu.hostname.split('.')[0];
+    
+    const peer = hsync.con.getPeer(hsyncPeer.host);
+    peerCons[hsyncPeer.hsu.hostname] = peerCons[hsyncPeer.hsu.hostname] || peer;
+
+    // console.log('HsyncP2POut', hsync.hsyncServer, '→', hsyncPeer.host, peer.dcOpen, peer.rtcStatus);
+
+
+
+    if (peer.dcOpen) {
+      // console.log('dcOpen already open', hsyncPeer.host);
+      const text = `${peerShortName} → ${conShortName}`;
+      node.status({fill:'green',shape:'dot',text});
+    }
+
+    peer.rtcEvents.on('dcOpen', () => { 
+      // console.log('dcOpen to', hsyncPeer.host);
+      const text = `${conShortName} → ${peerShortName}`;
+      node.status({fill:'green',shape:'dot',text});
+    });
+
+    if (peer.rtcStatus === 'error') {
+      // console.log('RTC ERROR', hsyncPeer.host);
+      node.status({fill:'red',shape:'ring',text:'error'});
+    }
+
+    if (peer.rtcStatus === 'connecting') {
+      // console.log('RTC CONNECTING', hsyncPeer.host);
+      node.status({fill:'yellow',shape:'ring',text:'connecting...'});
+    }
+
+    peer.rtcEvents.on('closed', () => {
+      node.status({fill:'red',shape:'ring',text:'closed'});
+    });
+
+    peer.rtcEvents.on('disconnected', () => {
+      node.status({fill:'red',shape:'ring',text:'disconnected'});
+    });
+
+    if (!peer.dcOpen && peer.rtcStatus !== 'connecting') {
+      node.status({fill:'yellow',shape:'ring',text:'connecting...'});
+      console.log('connecting rtc');
+      peer.connectRTC()
+      .then(() => {
+        console.log('then, connected rtc');
+      })
+      .catch((e) => {
+        console.log('err, connect rtc', e);
+        node.error(e);
+      });
+    }
+
+    node.on('input', async function(msg) {
+      console.log('hsync-p2p-in', msg, peer.dcOpen);
+      if (peer.dcOpen) {
+        if (!msg.from) {
+          msg.from = hsync.con.hsyncServer;
+        }
+        if (!msg.topic) {
+          msg.topic = 'json RTC msg'
+        }
+        peer.sendJSONMsg(msg);
+      }
+    });
+    
+    hsync.on('error', () => {
+      node.status({fill:'red',shape:'ring',text:'error'});
+    });
+
+    // hsync.con.on('connected', () => {
+    //   let host = 'connected';
+    //   try {
+    //     host = new URL(hsync.con.hsyncServer).hostname;
+    //   } catch (e) {}
+    //   node.status({fill:'green',shape:'dot',text: host});
+    // });
+  }
+  RED.nodes.registerType('hs-p2p-out', HsyncP2POut);
 }
